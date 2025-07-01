96dd2a5779fa9ec8eada5bd166860c726027f2a0
Kaizu-r <jljoloc@gmail.com>
2025-07-02 00:28:42 +0800

    ADD: setPixel declaration

--- headers/screen.hpp
+++ headers/screen.hpp
@@ -2,13 +2,17 @@
 #define SCREEN_H
 
 #include <iostream>
+#include "matrix.hpp"
 
 class Screen{
 public:
     int width;
     int height;
 
+    char* pixels;
+
     Screen(int width, int height);
+    void setPixel(char pixel, vec2 position);
 };
 
 
